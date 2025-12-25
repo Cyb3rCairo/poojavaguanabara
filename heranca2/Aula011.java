5940ec8a06c7a12fe523a38a93f995424a4da637
Cyb3rCairo <devcairoalmeida@gmail.com>
2025-12-24 23:13:54 -0300

    update

---
+++ heranca2/Aula011.java
@@ -0,0 +1,9 @@
+public class Aula011 {
+    public static void main(String[] args) {
+        
+        Aluno a1 = new Aluno();
+        a1.setNome("Carlos");
+        a1.pagarMensalidade();
+
+    }
+}
